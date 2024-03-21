96ac6f409b7398d9160a77369cc050ce51f673a5
DuongHaoNika <hao510duong@gmail.com>
2024-03-21 15:34:41 +0700

    fix some bug

--- src/helper/initdata.js
+++ src/helper/initdata.js
@@ -22,33 +22,34 @@ async function main() {
           userid: createUser.id,
       }
     })
-    const photo = await prisma.photo.create({
-      data: {
-        url: faker.image.url(),
-      }
-    })
 
-    const video = await prisma.video.create({
-      data: {
-        url: faker.music.songName()
-      }
-    })
-  
     const post = await prisma.post.create({
       data: {
-        authorid: createUser.id,
-        content: faker.string.alpha(),
-        photoId: photo.id,
-        videoId: video.id
+        userId: createUser.id,
+        content: "Hello " + faker.person.fullName(),
       }
     })
 
     const story = await prisma.story.create({
       data: {
-        authorid: createUser.id,
-        content: faker.string.alpha(),
-        photoId: photo.id,
-        videoId: video.id
+        userId: createUser.id,
+        content: "Hello " + faker.person.fullName(),
+      }
+    })
+
+    const photo = await prisma.photo.create({
+      data: {
+        url: faker.image.url(),
+        postId: post.id,
+        storyId: story.id
+      }
+    })
+
+    const video = await prisma.video.create({
+      data: {
+        url: faker.music.songName(),
+        postId: post.id,
+        storyId: story.id
       }
     })
   }
