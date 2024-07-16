210059a06bae41b4e5c549b09c820c83cff29864
Đặng Khánh AT18 A <kelvinkhanh192@gmail.com>
2024-07-16 14:41:02 +0700

    fixSSTI

--- src/controller/homeControllers.js
+++ src/controller/homeControllers.js
@@ -2,26 +2,52 @@ import {PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 import ogs from 'open-graph-scraper';
 import moment from 'moment-timezone';
-import axios from 'axios';
+import axios from 'axios'
+
+const getLastestId = async function() {
+    const LastestId = await prisma.post.findMany({
+      orderBy: {
+        id: 'desc',
+      },
+      take: 1,
+    });
+    if (LastestId.length > 0) {
+      return LastestId[0].id;
+    } else {
+      return 1;
+    }
+}
 
 const getHomePage = async (req,res) => {
     try {
-        //fetch data status
+        
+    } catch (error) {
+        
+    }
+
+    try {
+        //data status have comment
         let data1 = await prisma.$queryRaw`
         SELECT * FROM \"post\" INNER JOIN \"user_info\" ON post.authorid=user_info.userid WHERE viewingobject='Public' ORDER BY post.id DESC`
+        
+        //data status no comment (handle XSS vul)
         let data4 = await prisma.$queryRaw`
         SELECT * FROM \"post\" INNER JOIN \"user_info\" ON post.authorid=user_info.userid WHERE viewingobject='Public' ORDER BY post.id DESC`
-        //fetch my data
+        
+        //my data (name + avatar)
         let data2 = await prisma.$queryRaw`SELECT * FROM \"user_info\" WHERE userid=${req.decoded.id}`
-        //fetch comment data
+        
+        //code fetch comment data
         for (let i = 0; i < data1.length; ++i) {
+            //data3 is comment (add data3 to data1)
             let data3 = await prisma.$queryRaw`
             SELECT * FROM "post_comment" INNER JOIN "user_info" ON post_comment.authorid=user_info.userid WHERE postid=${data1[i].id} ORDER BY commentid ASC`
             if (data3[0]){
                 data1[i].comment = data3
             }
         }
-        //time
+
+        //handle time
         const now = new Date()
         for (let i = 0; i < data1.length; ++i) {
             const specificTime = new Date(data1[i].create_at);
@@ -54,7 +80,7 @@ const getHomePage = async (req,res) => {
 const handleHome = async (req,res) =>{
     try {
         const [setting] = await prisma.$queryRaw`Select status from vulnerable where name='SSRF'`
-        const content = await req.body.content
+        const content = await req.body.contentstatus
         const urlRegex = /(https?:\/\/[^\s]+)/g
         const urls = content.match(urlRegex);
         let url = 'None', html6
@@ -106,13 +132,22 @@ const handleHome = async (req,res) =>{
             }
         }
         const currentTime = moment().toISOString()
-        await prisma.$queryRaw`INSERT INTO \"post\" (authorid, content, create_at, feeling, checkin, image, video, viewingobject, url, view_image, description) 
-        VALUES (${req.decoded.id}, ${content}, ${currentTime}, 'None', 'None', 'None', 'None', 'Public', ${url}, ${view_image}, ${description});`
+        let LastestId = await getLastestId() + 1
+
+        //xml file
+        let document_data = "None", document_name = "None"
+        if (req.file) {
+            document_data = req.file.buffer.toString('utf-8')
+            document_name = req.file.originalname
+        }
+        await prisma.$queryRaw`INSERT INTO \"post\" (id, authorid, content, create_at, feeling, checkin, image, video, document_name, document_data, viewingobject, url, view_image, description) 
+        VALUES (${LastestId}, ${req.decoded.id}, ${content}, ${currentTime}, 'None', 'None', 'None', 'None', ${document_name}, ${document_data}, 'Public', ${url}, ${view_image}, ${description});`
         if (setting.status === 'Hard') {
             return res.send(html6);
         }
+        res.send('ok')
     } catch (error) {
-        //console.error("Error: ", error.message);
+        console.error("Error: ", error);
         return res.status(500).send('Internal Server Error');
     }
 }
--- src/controller/statuspostControllers.js
+++ src/controller/statuspostControllers.js
@@ -1,5 +1,7 @@
 import { prisma } from '../config/prisma.js';
 import pug from 'pug';
+import libxmljs from 'libxmljs'
+import { XMLParser } from 'fast-xml-parser';
 
 const getStatusPage = async (req,res) => {
     const { id } = req.query
@@ -11,34 +13,39 @@ const getStatusPage = async (req,res) => {
         return res.render('timelineerror', {data: "id is not valid"})
 
     try {
-        //fetch data name and avatar
-        const data = await prisma.post.findUnique({
-            where: {
-            id: id1,
-            },
-        })
-        if (data === null)
-            next()
+        const [setting] = await prisma.$queryRaw`Select status from vulnerable where name='SSTI'`
 
-        //fetch data status
+        //data status have comment
         let data1 = await prisma.$queryRaw`
         SELECT * FROM \"post\" INNER JOIN \"user_info\" ON post.authorid=user_info.userid WHERE post.id=${id1}` 
+        
+        //data status no comment (handle XSS vul)
         let data4 = await prisma.$queryRaw`
         SELECT * FROM \"post\" INNER JOIN \"user_info\" ON post.authorid=user_info.userid WHERE post.id=${id1}` 
-        //fetch my data
+        
+        //my data (name + avatar)
         let data2 = await prisma.$queryRaw`SELECT * FROM \"user_info\" WHERE userid=${req.decoded.id}`
-        //fetch comment data
+        
+        //code fetch comment data
         for (let i = 0; i < data1.length; ++i) {
+            //data3 is comment (add data3 to data1)
             let data3 = await prisma.$queryRaw`
             SELECT * FROM "post_comment" INNER JOIN "user_info" ON post_comment.authorid=user_info.userid WHERE postid=${data1[i].id} ORDER BY commentid ASC`
             if (data3[0]) {
                 data1[i].comment = data3
-                for (let j = 0; j < data3.length; ++j) {
-                    data3[j].content = pug.render(`|${data3[j].content}`)
+                if (setting.status === 'Medium'){
+                    for (let j = 0; j < data3.length; ++j) {
+                        try {
+                            //code vul SSTI in post comment
+                            data3[j].content = pug.render(`|${data3[j].content}`)
+                        } catch (error) {
+                        }
+                    }
                 }
             }
         }
-        //time
+
+        //handle time
         const now = new Date()
         for (let i = 0; i < data1.length; ++i) {
             const specificTime = new Date(data1[i].create_at);
@@ -60,7 +67,13 @@ const getStatusPage = async (req,res) => {
                 data1[i].post_time = `${seconds} seconds ago`;
             }
         }
-        return res.render('statuspost1', {data, data1, data2: data2[0], data4})
+        if (setting.status === 'Medium'){
+            //Have vul, after commenting will refresh the page (statuspost1)
+            return res.render('statuspost1', {data1, data2: data2[0], data4})
+        }
+        else {
+            return res.render('statuspost', {data1, data2: data2[0], data4})
+        }
         
     } catch (error) {
         //console.error("Error: ", error.message);
@@ -68,4 +81,56 @@ const getStatusPage = async (req,res) => {
     }
 }
 
-export default {getStatusPage}
+const fileXml = async (req,res) => {
+    const { id } = req.query
+    if (!id) {
+        return res.render('timelineerror', {data: "Missing id parameter"})
+    }
+    const id1 = Number(id)
+    if (isNaN(id1))
+        return res.render('timelineerror', {data: "id is not valid"})
+
+    try {
+        const result = await prisma.post.findUnique({
+            where: {
+            id: id1,
+            },
+        })
+        if (result === null)
+            next()
+
+        try {
+            let data
+            const [setting] = await prisma.$queryRaw`Select status from vulnerable where name='XXE'`
+            if (setting.status === 'Easy' || setting.status === 'Medium' ){
+                const xmlDoc = libxmljs.parseXml(result.document_data, {
+                    replaceEntities: true,  
+                    dtdload: true 
+                });
+            }
+            else {
+                const options = {
+                    ignoreDTD: true,
+                    ignoreEntityReferences: true,
+                    entityProcessors: {
+                        generalEntity: { maxRepetition: 10 },
+                        parameterEntity: { maxRepetition: 10 }
+                    }
+                };
+                const parser = new XMLParser(options);
+                const jsonObj = await parser.parse(result.document_data);
+                data = JSON.stringify(jsonObj, null, 2)
+            }
+
+            return res.status(200).send(data);
+        } catch (parseError) {
+            console.log(parseError)
+            return res.status(500).send('Error parsing XML file.');
+        }
+
+    } catch (error) {
+        return res.render('timelineerror', {data: "File not found"})
+    }
+}
+
+export default {getStatusPage, fileXml}
